17ed2c9bb46ad10dfdfedcb498f9686881fb6f77
Rhuan Queiroz <rhuancarlos.queiroz@gmail.com>
2022-02-28 18:56:37 -0300

    Add Flake8 linter workflow

--- src/app.py
+++ src/app.py
@@ -1,11 +1,11 @@
-from flask import Flask, request
+from flask import Flask
 from flask_restful import Api
 from resources.hello_world import HelloWorld
 
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(HelloWorld, '/hello')
+api.add_resource(HelloWorld, "/hello")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
--- src/resources/hello_world.py
+++ src/resources/hello_world.py
@@ -1,6 +1,7 @@
 from flask import jsonify
 from flask_restful import Resource
 
+
 class HelloWorld(Resource):
     def get(self):
-        return jsonify({'Hello': "World!"})
+        return jsonify({"Hello": "World!"})
