830776e23cc8e15cb5f790a3497eb6e727b95d2f
Lucas Pimentel <luk.2001@hotmail.com>
2022-03-18 00:24:22 -0300

    Adds new constants
    Co-authored-by: Thiago Lopes <thiago.lopes.santos.tls@gmail.com>
    Co-authored-by: Rhuan Carlos <Rhuancpq@users.noreply.github.com>
    Co-authored-by: joao-henrique10 <joao-henrique10@hotmail.com>

--- src/core/constants.py
+++ src/core/constants.py
@@ -1,4 +1,4 @@
-AVAILABLE_PRE_CONFIGS = {
+""" AVAILABLE_PRE_CONFIGS = {
     "reliability": {
         "name": "Reliability",
         "subcharacteristics": {
@@ -20,9 +20,70 @@ AVAILABLE_PRE_CONFIGS = {
                 "measures": {
                     "non_complex_file_density": {"name": "Non complex file density"},
                     "commented_file_density": {"name": "Commented file density"},
-                    "duplication_absense": {"name": "Duplicatio abscense"},
+                    "duplication_absense": {"name": "Duplication abscense"},
                 },
             }
+        },
+    },
+} """
+
+AVAILABLE_PRE_CONFIGS = {
+    "characteristics": {
+        "reliability": {
+            "name": "Reliability",
+            "subcharacteristics": ["testing_status"],
+        },
+        "maintainability": {
+            "name": "Maintainability",
+            "subcharacteristics": ["modifiability"],
+        },
+    },
+    "subcharacteristics": {
+        "testing_status": {
+            "name": "Testing Status",
+            "measures": ["passed_tests", "test_builds", "test_coverage"],
+            "characteristics": ["reliability"],
+        },
+        "modifiability": {
+            "name": "Modifiability",
+            "measures": [
+                "non_complex_file_density",
+                "commented_file_density",
+                "duplication_absense",
+            ],
+            "characteristics": ["maintainability"],
+        },
+    },
+    "measures": {
+        "passed_tests": {
+            "name": "Passed Tests",
+            "subcharacteristics": ["testing_status"],
+            "characteristics": ["reliability"],
+        },
+        "test_builds": {
+            "name": "Test Builds",
+            "subcharacteristics": ["testing_status"],
+            "characteristics": ["reliability"],
+        },
+        "test_coverage": {
+            "name": "Test Coverage",
+            "subcharacteristics": ["testing_status"],
+            "characteristics": ["reliability"],
+        },
+        "non_complex_file_density": {
+            "name": "Non complex file density",
+            "subcharacteristics": ["modifiability"],
+            "characteristics": ["maintainability"],
+        },
+        "commented_file_density": {
+            "name": "Commented file density",
+            "subcharacteristics": ["modifiability"],
+            "characteristics": ["maintainability"],
+        },
+        "duplication_absense": {
+            "name": "Duplication abscense",
+            "subcharacteristics": ["modifiability"],
+            "characteristics": ["maintainability"],
         },
     },
 }
