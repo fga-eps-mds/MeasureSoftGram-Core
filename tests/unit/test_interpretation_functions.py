2c441f8ae8bca10da4325b209df3a33c7b80f8b0
igor-paiva <igorbatistapaiva@outlook.com>
2022-03-19 01:55:31 -0300

    Refactor interpretation functions import

--- tests/unit/test_interpretation_functions.py
+++ tests/unit/test_interpretation_functions.py
@@ -1,4 +1,8 @@
-from src.core import interpretation_functions as functions
+from src.core.interpretation_functions import (
+    non_complex_files_density,
+    commented_files_density,
+    absence_of_duplications
+)
 from src.core.exceptions import InvalidMetricValue, InvalidInterpretationFunctionArguments
 from tests.test_helpers import create_file_df
 import pandas as pd
@@ -7,51 +11,51 @@ import pytest
 
 
 INVALID_METRICS_TEST_DATA = [
-    (functions.non_complex_files_density,
+    (non_complex_files_density,
         'tests/unit/data/zero_number_of_files.json', 'The number of files is lesser or equal than 0'),
-    (functions.non_complex_files_density, 'tests/unit/data/zero_number_of_functions.json',
+    (non_complex_files_density, 'tests/unit/data/zero_number_of_functions.json',
         'The number of functions of all files is lesser or equal than 0'),
-    (functions.non_complex_files_density, 'tests/unit/data/zero_cyclomatic_complexity.json',
+    (non_complex_files_density, 'tests/unit/data/zero_cyclomatic_complexity.json',
         'The cyclomatic complexity of all files is lesser or equal than 0'),
-    (functions.commented_files_density,
+    (commented_files_density,
         'tests/unit/data/zero_number_of_files.json', 'The number of files is lesser or equal than 0'),
-    (functions.absence_of_duplications,
+    (absence_of_duplications,
         'tests/unit/data/zero_number_of_files.json', 'The number of files is lesser or equal than 0'),
 ]
 
 
 SUCCESS_TEST_DATA = [
-    (functions.non_complex_files_density,
+    (non_complex_files_density,
         'tests/unit/data/fga-eps-mds_2021-2-SiGeD-Frontend-03-15-2022-23_57_08.json', 0.688622754491018),
-    (functions.non_complex_files_density,
+    (non_complex_files_density,
         'tests/unit/data/fga-eps-mds-2020_2-Projeto-Kokama-Usuario-17-04-2021.json', 1.0),
-    (functions.non_complex_files_density,
+    (non_complex_files_density,
         'tests/unit/data/fga-eps-mds-2020_2-Lend.it-Raleway-user-13-04-2021.json', 0.6428571428571429),
-    (functions.commented_files_density,
+    (commented_files_density,
         'tests/unit/data/fga-eps-mds-2020_2-Projeto-Kokama-Usuario-17-04-2021.json', 0.0),
-    (functions.commented_files_density,
+    (commented_files_density,
         'tests/unit/data/fga-eps-mds_2021-2-SiGeD-Frontend-03-15-2022-23_57_08.json', 0.005988023952095809),
-    (functions.commented_files_density,
+    (commented_files_density,
         'tests/unit/data/fga-eps-mds-2020_2-Lend.it-Raleway-user-13-04-2021.json', 0.0),
-    (functions.absence_of_duplications,
+    (absence_of_duplications,
         'tests/unit/data/fga-eps-mds_2021-2-SiGeD-Frontend-03-15-2022-23_57_08.json', 0.9101796407185628),
-    (functions.absence_of_duplications,
+    (absence_of_duplications,
         'tests/unit/data/fga-eps-mds-2020_2-Projeto-Kokama-Usuario-17-04-2021.json', 1.0),
-    (functions.absence_of_duplications,
+    (absence_of_duplications,
         'tests/unit/data/fga-eps-mds-2020_2-Lend.it-Raleway-user-13-04-2021.json', 1.0)
 ]
 
 
 INVALID_ARGUMENTS_TEST_DATA = [
-    (functions.non_complex_files_density, None),
-    (functions.non_complex_files_density, False),
-    (functions.non_complex_files_density, pd.Series(data={'a': 1, 'b': 2, 'c': 3}, index=['a', 'b', 'c'])),
-    (functions.commented_files_density, None),
-    (functions.commented_files_density, False),
-    (functions.commented_files_density, pd.Series(data={'a': 1, 'b': 2, 'c': 3}, index=['a', 'b', 'c'])),
-    (functions.absence_of_duplications, None),
-    (functions.absence_of_duplications, False),
-    (functions.absence_of_duplications, pd.Series(data={'a': 1, 'b': 2, 'c': 3}, index=['a', 'b', 'c']))
+    (non_complex_files_density, None),
+    (non_complex_files_density, False),
+    (non_complex_files_density, pd.Series(data={'a': 1, 'b': 2, 'c': 3}, index=['a', 'b', 'c'])),
+    (commented_files_density, None),
+    (commented_files_density, False),
+    (commented_files_density, pd.Series(data={'a': 1, 'b': 2, 'c': 3}, index=['a', 'b', 'c'])),
+    (absence_of_duplications, None),
+    (absence_of_duplications, False),
+    (absence_of_duplications, pd.Series(data={'a': 1, 'b': 2, 'c': 3}, index=['a', 'b', 'c']))
 ]
 
 
