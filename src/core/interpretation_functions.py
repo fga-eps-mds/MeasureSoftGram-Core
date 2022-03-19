e2aa76eee0771418d4663a7a2dc426e2e68abb18
Frans6 <jefferson.frds@gmail.com>
2022-03-18 22:09:28 -0300

    Create commented files density function
    Co-authored-by: Marcos574 <mvdeus574@gmail.com>
    Co-authored-by: Slimow <170114139@aluno.unb.br>
    Co-authored-by: igor-paiva <igorbatistapaiva@outlook.com>

--- src/core/interpretation_functions.py
+++ src/core/interpretation_functions.py
@@ -31,3 +31,32 @@ def non_complex_files_density(data_frame):
     files_beneath_threshold_df = data_frame[(files_complexity / files_functions) < CYCLOMATIC_COMPLEXITY_THRESHOLD]
 
     return len(files_beneath_threshold_df) / number_of_files
+
+
+def commented_files_density(data_frame):
+    """
+    Calculates commented files density (m2).
+
+    This function calculates commented files density measure (m2)
+    used to assess the changeability quality subcharacteristic.
+    """
+
+    MINIMUM_COMMENT_DENSITY_THRESHOLD = 10
+    MAXIMUM_COMMENT_DENSITY_THRESHOLD = 30
+
+    if not isinstance(data_frame, pd.DataFrame):
+        raise InvalidInterpretationFunctionArguments('Expected data_frame to be a pandas.DataFrame')
+
+    files_comment_lines_density = data_frame['comment_lines_density'].astype(float)
+    number_of_files = len(data_frame)
+
+    if number_of_files <= 0:
+        raise InvalidMetricValue('The number of files is lesser or equal than 0')
+
+    if files_comment_lines_density.sum() < 0:
+        raise InvalidMetricValue('The number of files comment lines density is lesser than 0')
+
+    files_between_thresholds_df = data_frame[files_comment_lines_density.between(
+        MINIMUM_COMMENT_DENSITY_THRESHOLD, MAXIMUM_COMMENT_DENSITY_THRESHOLD, inclusive=True)]
+
+    return len(files_between_thresholds_df) / number_of_files
