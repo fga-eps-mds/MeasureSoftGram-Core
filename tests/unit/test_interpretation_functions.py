850fdba56691acad721270d6f8a66476d96e6e2c
Frans6 <jefferson.frds@gmail.com>
2022-03-18 21:53:58 -0300

    Add more test cases to interpretation functions test
    Co-authored-by: Slimow <170114139@aluno.unb.br>
    Co-authored-by: Marcos574 <mvdeus574@gmail.com>
    Co-authored-by: igor-paiva <igorbatistapaiva@outlook.com>

--- tests/unit/test_interpretation_functions.py
+++ tests/unit/test_interpretation_functions.py
@@ -1,4 +1,4 @@
-from src.core.interpretation_functions import non_complex_files_density
+from src.core.interpretation_functions import non_complex_files_density, commented_files_density, absence_duplications
 from src.core.exceptions import InvalidMetricValue, InvalidInterpretationFunctionArguments
 from tests.test_helpers import create_file_df
 import pandas as pd
@@ -6,50 +6,73 @@ from glob import glob
 import pytest
 
 
-ERROR_TEST_DATA = [
-    ('tests/unit/data/zero_number_of_files.json', 'The number of files is lesser or equal than 0'),
-    ('tests/unit/data/zero_number_of_functions.json', 'The number of functions of all files is lesser or equal than 0'),
-    ('tests/unit/data/zero_cyclomatic_complexity.json', 'The cyclomatic complexity of all files is lesser or equal than 0')
+INVALID_METRICS_TEST_DATA = [
+    (non_complex_files_density, 'tests/unit/data/zero_number_of_files.json', 'The number of files is lesser or equal than 0'),
+    (non_complex_files_density, 'tests/unit/data/zero_number_of_functions.json',
+     'The number of functions of all files is lesser or equal than 0'),
+    (non_complex_files_density, 'tests/unit/data/zero_cyclomatic_complexity.json',
+     'The cyclomatic complexity of all files is lesser or equal than 0'),
+    (commented_files_density, 'tests/unit/data/zero_number_of_files.json',
+     'The number of files is lesser or equal than 0'),
+    (absence_duplications, 'tests/unit/data/zero_number_of_files.json',
+     'The number of files is lesser or equal than 0'),
 ]
 
 
 SUCCESS_TEST_DATA = [
-    ('tests/unit/data/fga-eps-mds_2021-2-SiGeD-Frontend-03-15-2022-23_57_08.json', 0.688622754491018),
-    ('tests/unit/data/fga-eps-mds-2020_2-Projeto-Kokama-Usuario-17-04-2021.json', 1.0)
+    (non_complex_files_density, 'tests/unit/data/fga-eps-mds_2021-2-SiGeD-Frontend-03-15-2022-23_57_08.json', 0.688622754491018),
+    (non_complex_files_density, 'tests/unit/data/fga-eps-mds-2020_2-Projeto-Kokama-Usuario-17-04-2021.json', 1.0),
+    (non_complex_files_density, 'tests/unit/data/fga-eps-mds-2020_2-Lend.it-Raleway-user-13-04-2021.json', 0.6428571428571429),
+    (commented_files_density, 'tests/unit/data/fga-eps-mds-2020_2-Projeto-Kokama-Usuario-17-04-2021.json', 0.0),
+    (commented_files_density, 'tests/unit/data/fga-eps-mds_2021-2-SiGeD-Frontend-03-15-2022-23_57_08.json', 0.005988023952095809),
+    (commented_files_density, 'tests/unit/data/fga-eps-mds-2020_2-Lend.it-Raleway-user-13-04-2021.json', 0.0),
+    (absence_duplications, 'tests/unit/data/fga-eps-mds_2021-2-SiGeD-Frontend-03-15-2022-23_57_08.json', 0.9101796407185628),
+    (absence_duplications, 'tests/unit/data/fga-eps-mds-2020_2-Projeto-Kokama-Usuario-17-04-2021.json', 1.0),
+    (absence_duplications, 'tests/unit/data/fga-eps-mds-2020_2-Lend.it-Raleway-user-13-04-2021.json', 1.0)
 ]
 
 
 INVALID_ARGUMENTS_TEST_DATA = [
-    None,
-    False,
-    pd.Series(data={'a': 1, 'b': 2, 'c': 3}, index=['a', 'b', 'c'])
+    (non_complex_files_density, None),
+    (non_complex_files_density, False),
+    (non_complex_files_density, pd.Series(data={'a': 1, 'b': 2, 'c': 3}, index=['a', 'b', 'c'])),
+    (commented_files_density, None),
+    (commented_files_density, False),
+    (commented_files_density, pd.Series(data={'a': 1, 'b': 2, 'c': 3}, index=['a', 'b', 'c'])),
+    (absence_duplications, None),
+    (absence_duplications, False),
+    (absence_duplications, pd.Series(data={'a': 1, 'b': 2, 'c': 3}, index=['a', 'b', 'c']))
 ]
 
 
-@pytest.mark.parametrize("file_path,error_msg", ERROR_TEST_DATA)
-def test_non_complex_files_density_error_metrics(file_path, error_msg):
+@pytest.mark.parametrize("interpretation_func,file_path,error_msg", INVALID_METRICS_TEST_DATA)
+def test_interpretation_functions_invalid_metrics(interpretation_func, file_path, error_msg):
     json = glob(file_path)
 
     with pytest.raises(InvalidMetricValue) as error:
-        non_complex_files_density(create_file_df(json))
+        interpretation_func(create_file_df(json))
 
-    assert str(error.value) == error_msg
+    assert str(
+        error.value) == error_msg, f"Expected: {interpretation_func.__name__}(create_file_df('{file_path.split('/')[-1]}')) to raise an InvalidMetricValue('{error_msg}')"
 
 
-@pytest.mark.parametrize("file_path,expected_result", SUCCESS_TEST_DATA)
-def test_non_complex_files_density(file_path, expected_result):
+@pytest.mark.parametrize("interpretation_func,file_path,expected_result", SUCCESS_TEST_DATA)
+def test_interpretation_functions_success(interpretation_func, file_path, expected_result):
     json = glob(file_path)
 
-    result = non_complex_files_density(create_file_df(json))
+    result = interpretation_func(create_file_df(json))
 
-    assert result == expected_result
-    assert 0 <= result <= 1.0
+    assert result == expected_result, f"Expected: {interpretation_func.__name__}(create_file_df('{file_path.split('/')[-1]}')) == {expected_result}"
+    assert 0 <= result <= 1.0, f"Expected: 0 <= {interpretation_func.__name__}(create_file_df('{file_path.split('/')[-1]}')) <= 1.0"
 
 
-@pytest.mark.parametrize("data_frame", INVALID_ARGUMENTS_TEST_DATA)
-def test_non_complex_files_density_error_arguments(data_frame):
+@pytest.mark.parametrize("interpretation_func,data_frame", INVALID_ARGUMENTS_TEST_DATA)
+def test_non_complex_files_density_error_arguments(interpretation_func, data_frame):
 
     with pytest.raises(InvalidInterpretationFunctionArguments) as error:
-        non_complex_files_density(data_frame)
+        interpretation_func(data_frame)
 
-    assert str(error.value) == 'Expected data_frame to be a pandas.DataFrame'
+    str_value = 'pd.Series()' if isinstance(data_frame, pd.Series) else data_frame
+
+    assert str(error.value) == 'Expected data_frame to be a pandas.DataFrame', f"Expected: {interpretation_func.__name__}({str_value}) to raise an InvalidInterpretationFunctionArguments"
+
