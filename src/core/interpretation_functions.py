0ca2cc74a42fcff25de78c7bcbad43ef85ac8743
Frans6 <jefferson.frds@gmail.com>
2022-03-18 22:56:19 -0300

    Refactor interpretation functions

--- src/core/interpretation_functions.py
+++ src/core/interpretation_functions.py
@@ -2,6 +2,16 @@ from .exceptions import InvalidMetricValue, InvalidInterpretationFunctionArgumen
 import pandas as pd
 
 
+def check_arguments(data_frame):
+    if not isinstance(data_frame, pd.DataFrame):
+        raise InvalidInterpretationFunctionArguments('Expected data_frame to be a pandas.DataFrame')
+
+
+def check_number_of_files(number_of_files):
+    if number_of_files <= 0:
+        raise InvalidMetricValue('The number of files is lesser or equal than 0')
+
+
 def non_complex_files_density(data_frame):
     """
     Calculates non-complex files density (m1).
@@ -12,15 +22,13 @@ def non_complex_files_density(data_frame):
 
     CYCLOMATIC_COMPLEXITY_THRESHOLD = 10
 
-    if not isinstance(data_frame, pd.DataFrame):
-        raise InvalidInterpretationFunctionArguments('Expected data_frame to be a pandas.DataFrame')
+    check_arguments(data_frame)
 
     files_complexity = data_frame['complexity'].astype(float)
     files_functions = data_frame['functions'].astype(float)
     number_of_files = len(data_frame)
 
-    if number_of_files <= 0:
-        raise InvalidMetricValue('The number of files is lesser or equal than 0')
+    check_number_of_files(number_of_files)
 
     if files_complexity.sum() <= 0:
         raise InvalidMetricValue('The cyclomatic complexity of all files is lesser or equal than 0')
@@ -44,14 +52,12 @@ def commented_files_density(data_frame):
     MINIMUM_COMMENT_DENSITY_THRESHOLD = 10
     MAXIMUM_COMMENT_DENSITY_THRESHOLD = 30
 
-    if not isinstance(data_frame, pd.DataFrame):
-        raise InvalidInterpretationFunctionArguments('Expected data_frame to be a pandas.DataFrame')
+    check_arguments(data_frame)
 
     files_comment_lines_density = data_frame['comment_lines_density'].astype(float)
     number_of_files = len(data_frame)
 
-    if number_of_files <= 0:
-        raise InvalidMetricValue('The number of files is lesser or equal than 0')
+    check_number_of_files(number_of_files)
 
     if files_comment_lines_density.sum() < 0:
         raise InvalidMetricValue('The number of files comment lines density is lesser than 0')
@@ -72,14 +78,12 @@ def absence_of_duplications(data_frame):
 
     DUPLICATED_LINES_THRESHOLD = 5.0
 
-    if not isinstance(data_frame, pd.DataFrame):
-        raise InvalidInterpretationFunctionArguments('Expected data_frame to be a pandas.DataFrame')
+    check_arguments(data_frame)
 
     files_duplicated_lines_density = data_frame['duplicated_lines_density'].astype(float)
     number_of_files = len(data_frame)
 
-    if number_of_files <= 0:
-        raise InvalidMetricValue('The number of files is lesser or equal than 0')
+    check_number_of_files(number_of_files)
 
     if files_duplicated_lines_density.sum() < 0:
         raise InvalidMetricValue('The number of files duplicated lines density is lesser than 0')
