5f8c6c69959f36aac1f0c5e5210a49018dd3bee2
Joao Henrique <jhcalzavara@hotmail.com>
2022-03-31 22:59:33 -0300

    Adds wheighting function
    Co-authored-by: anabfs <anna.bf.santos1@gmail.com>
    Co-authored-by: Frans6 <jefferson.frds@gmail.com>

--- src/core/exceptions.py
+++ src/core/exceptions.py
@@ -1,14 +1,22 @@
-
 class MeasureSoftGramCoreException(Exception):
     """Base MeasureSoftGram Core exception"""
+
     pass
 
 
 class InvalidMetricValue(MeasureSoftGramCoreException):
     """Raised when a invalid metric value is provided to the interpretation function"""
+
     pass
 
 
 class InvalidInterpretationFunctionArguments(MeasureSoftGramCoreException):
     """Raised when invalid arguments are provided to an interpretation function"""
+
+    pass
+
+
+class InvalidWeightSize(MeasureSoftGramCoreException):
+    """Raised when the size of the weight and measures are not equal"""
+
     pass
---
+++ tests/unit/test_weighting.py
@@ -0,0 +1,37 @@
+from src.core.weighting import weighting_operation
+import pytest
+
+from src.core.exceptions import InvalidWeightSize
+
+
+# @pytest.mark.parametrize()
+# def test_weighting_operation():
+#     list_whatever = function_asda()
+#     tensor = create_sc_tensor(list_whatever, 1)
+#     res = operation_ponderacao(tensor)
+#     assert res == [[[0.23234132]], [[0.00202036]], [[0.30709461]]]
+
+
+def test_weighting_operation():
+    df = [0.30, 0.32, 0.12]
+    weights = [0.30, 0.45, 0.25]
+    res = weighting_operation(df, weights)
+
+    assert pytest.approx(res.tolist()) == [0.09, 0.144, 0.03]
+
+
+def test_weighting_operation_2():
+    df = [0.40, 0.50, 0.10]
+    weights = [0.30, 0.45, 0.25]
+    res = weighting_operation(df, weights)
+
+    assert pytest.approx(res.tolist()) == [0.12, 0.225, 0.025]
+
+
+def test_diference_size():
+    df = [0.40, 0.40, 0.10, 0.10]
+    weights = [0.30, 0.45, 0.25]
+    try:
+        weighting_operation(df, weights)
+    except InvalidWeightSize:
+        assert True
