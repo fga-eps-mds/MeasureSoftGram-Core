f82c210fe9e27db04c31dca380af80d6da497a01
Ana Beatriz Santos <anna.bf.santos1@gmail.com>
2022-04-04 01:40:23 -0300

    Fix weighting operation
    Co-authored-by: joao-henrique10 <jhcalzavara@gmail.com>
    Co-authored-by: Frans6 <jefferson.frds@gmail.com>

--- src/core/weighting.py
+++ src/core/weighting.py
@@ -1,7 +1,13 @@
 import numpy as np
+import pytest
+
+from src.core.exceptions import InvalidWeightSize
 
 
 def weighting_operation(df, weights):
-    df = np.array(df)
-    weights = np.array(weights)
-    return df * weights
+    if len(df) != len(weights):
+        raise InvalidWeightSize
+    else:
+        df = np.array(df)
+        weights = np.array(weights)
+        return df * weights
