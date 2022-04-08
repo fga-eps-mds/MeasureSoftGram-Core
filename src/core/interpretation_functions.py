517c69de0e1682a43f527bd70bbdb159057c05f3
igor-paiva <igorbatistapaiva@outlook.com>
2022-04-07 23:29:09 -0300

    Improve functions docstrings and variables names
    Co-authored-by: Rhuancpq <rhuancarlos.queiroz@gmail.com>

--- src/core/interpretation_functions.py
+++ src/core/interpretation_functions.py
@@ -4,6 +4,10 @@ import numpy as np
 
 
 def check_arguments(data_frame):
+    """
+    Raises an InvalidInterpretationFunctionArguments exception if argument it's not a pandas.DataFrame.
+    """
+
     if not isinstance(data_frame, pd.DataFrame):
         raise InvalidInterpretationFunctionArguments(
             "Expected data_frame to be a pandas.DataFrame"
@@ -11,6 +15,10 @@ def check_arguments(data_frame):
 
 
 def check_number_of_files(number_of_files):
+    """
+    Raises an InvalidMetricValue exception if the number of files is lesser or equal than 0.
+    """
+
     if number_of_files <= 0:
         raise InvalidMetricValue("The number of files is lesser or equal than 0")
 
@@ -37,9 +45,9 @@ def create_coordinate_pair(min_threshhold, max_threshold):
 
 def non_complex_files_density(data_frame):
     """
-    Calculates non-complex files density (m1).
+    Calculates non-complex files density (em1).
 
-    This function calculates non-complex files density measure (m1)
+    This function calculates non-complex files density measure (em1)
     used to assess the changeability quality subcharacteristic.
     """
 
@@ -49,7 +57,7 @@ def non_complex_files_density(data_frame):
     files_complexity = data_frame["complexity"].astype(float)
     # files_functions = m2 metric
     files_functions = data_frame["functions"].astype(float)
-    # number_of_files = m3 metric
+    # number_of_files = Tm3 metric
     number_of_files = len(data_frame)
 
     check_number_of_files(number_of_files)
@@ -64,7 +72,6 @@ def non_complex_files_density(data_frame):
             "The number of functions of all files is lesser or equal than 0"
         )
 
-    # m0 =
     m0 = np.median(files_complexity / files_functions)
 
     x, y = create_coordinate_pair(0, m0)
@@ -78,9 +85,9 @@ def non_complex_files_density(data_frame):
 
 def commented_files_density(data_frame):
     """
-    Calculates commented files density (m2).
+    Calculates commented files density (em2).
 
-    This function calculates commented files density measure (m2)
+    This function calculates commented files density measure (em2)
     used to assess the changeability quality subcharacteristic.
     """
 
@@ -89,12 +96,10 @@ def commented_files_density(data_frame):
 
     check_arguments(data_frame)
 
-    # number_of_files = m3 metric
+    # number_of_files = Tm3 metric
     number_of_files = len(data_frame)
     # files_comment_lines_density = m4 metric
-    files_comment_lines_density = data_frame["comment_lines_density"].astype(
-        float
-    )  # m4
+    files_comment_lines_density = data_frame["comment_lines_density"].astype(float)
 
     check_number_of_files(number_of_files)
 
@@ -122,9 +127,9 @@ def commented_files_density(data_frame):
 
 def absence_of_duplications(data_frame):
     """
-    Calculates duplicated files absence (m3).
+    Calculates duplicated files absence (em3).
 
-    This function calculates the duplicated files absence measure (m3)
+    This function calculates the duplicated files absence measure (em3)
     used to assess the changeability quality subcharacteristic.
     """
 
@@ -136,7 +141,7 @@ def absence_of_duplications(data_frame):
     files_duplicated_lines_density = data_frame["duplicated_lines_density"].astype(
         float
     )
-    # number_of_files = m3 metric
+    # number_of_files = Tm3 metric
     number_of_files = len(data_frame)
 
     check_number_of_files(number_of_files)
@@ -152,6 +157,6 @@ def absence_of_duplications(data_frame):
         files_duplicated_lines_density < DUPLICATED_LINES_THRESHOLD
     ]
 
-    em2i = interpolate_series(files_below_threshold, x, y)
+    em3i = interpolate_series(files_below_threshold, x, y)
 
-    return np.sum(em2i) / number_of_files
+    return np.sum(em3i) / number_of_files
