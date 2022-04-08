fd31265c7190ad233024888c7853d1f3b06a4025
joao-henrique10 <71076129+joao-henrique10@users.noreply.github.com>
2022-04-08 20:46:59 -0300

    Removes not used class at exceptions.py

--- src/core/exceptions.py
+++ src/core/exceptions.py
@@ -20,9 +20,3 @@ class ValuesAndWeightsOfDifferentSizes(MeasureSoftGramCoreException):
     """Raised when the length of the weight and values are not equal"""
 
     pass
-
-
-class InvalidWeightingOperation(MeasureSoftGramCoreException):
-    """Raised when the weighting operation return a invalid result"""
-
-    pass
