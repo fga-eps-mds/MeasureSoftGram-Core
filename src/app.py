512055342880e2ce7140c2513598a448e9fae74c
VitorB2002 <vitorb.santos2002@gmail.com>
2022-04-22 21:49:44 -0300

    Add analysis routes
    Co-authored-by: Lucas Pimentel Quintao <LucasPimentel123@users.noreply.github.com>

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_restful import Api
 from src.resources.hello_world import HelloWorld
 from src.resources.available import AvailablePreConfigs
+from src.resources.analysis import Analysis
 
 
 app = Flask(__name__)
@@ -10,3 +11,5 @@ api = Api(app)
 api.add_resource(HelloWorld, "/hello")
 
 api.add_resource(AvailablePreConfigs, "/available-pre-configs")
+
+api.add_resource(Analysis, "/analysis")
