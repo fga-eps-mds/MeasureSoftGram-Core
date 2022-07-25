8c7bd8739baf0ec82b63d3c5a17208829f67278f
Herick Portugues <herick.portugues@gmail.com>
2022-07-25 01:24:49 -0300

    #139 Create schema for all supported measures

--- src/core/schemas.py
+++ src/core/schemas.py
@@ -5,3 +5,24 @@ class PassedTestsSchema(Schema):
     tests = fields.Float(required=True)
     test_errors = fields.Float(required=True)
     test_failures = fields.Float(required=True)
+
+
+class TestBuildsSchema(Schema):
+    test_execution_time = fields.Float(required=True)
+
+
+class TestCoverageSchema(Schema):
+    coverage = fields.Float(required=True)
+
+
+class NonComplexFileDensitySchema(Schema):
+    complexity = fields.Float(required=True)
+    functions = fields.Float(required=True)
+
+
+class CommentedFileDensitySchema(Schema):
+    comment_lines_density = fields.Float(required=True)
+
+
+class DuplicationAbsenceSchema(Schema):
+    duplicated_lines_density = fields.Float(required=True)
