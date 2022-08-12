1ba7bfe6b8f833216c421cb42ee7a91e0fb3047b
Herick Portugues <herick.portugues@gmail.com>
2022-08-12 13:22:09 -0300

    #204 Add function to calculate aggregated value and small corrections

--- src/core/analysis.py
+++ src/core/analysis.py
@@ -1,33 +1,26 @@
-from typing import Dict, List
+from typing import Dict, List, Tuple
 
 import pandas as pd
 from src.core.weighting import weighting_operation
-from src.core.agregation import agregation_operation
+from src.core.agregation import aggregation_operation
 from src.util.constants import MEASURES_INTERPRETATION_MAPPING
 
 
-def resolve_level(level_dict: dict, sublevel: dict, sublevel_key: str) -> dict:
-    aggregated_level = {}
-    all_weighted_values = {}
+def resolve_level(level_dict: dict, sublevel: dict, sublevel_key: str) -> Tuple[dict, dict]:
+    aggregated_level, all_weighted_values = {}, {}
     for key, value in level_dict.items():
         level_items = value[sublevel_key]
         level_weights = value["weights"]
 
-        weights_list = []
-        values_list = []
-
-        for idx in range(len(level_items)):
-            item = level_items[idx]
+        weights_list, values_list = [], []
+        for item in level_items:
             weights_list.append(level_weights[item])
             values_list.append(sublevel[item])
 
         weighted_items = weighting_operation(values_list, weights_list)
-        aggregated_value = agregation_operation(weighted_items, weights_list)
+        aggregated_value = aggregation_operation(weighted_items, weights_list)
 
-        weighted_items_dict = {}
-        for idx in range(len(level_items)):
-            item = level_items[idx]
-            weighted_items_dict[item] = weighted_items[idx]
+        weighted_items_dict = {item: weighted_items[idx] for idx, item in enumerate(level_items)}
 
         aggregated_level[key] = aggregated_value
         all_weighted_values[key] = weighted_items_dict
@@ -77,3 +70,9 @@ def calculate_measures(dataframe: pd.DataFrame, measures: List[str]) -> Dict[str
         combined_measures[measure] = MEASURES_INTERPRETATION_MAPPING[measure]["interpretation_function"](dataframe)
 
     return combined_measures
+
+
+def calculate_aggregated_value(values_list: List[float], weights_list: List[float]) -> float:
+    weighted_items = weighting_operation(values_list, weights_list)
+    aggregated_value = aggregation_operation(weighted_items, weights_list)
+    return aggregated_value
