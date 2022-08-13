125dea69286e4102f9d283f912ebefd162fd70b5
Durval Carvalho <durval.carvalho@brelo.com.br>
2022-08-13 13:49:33 -0300

    Add env var to control DEBUG mode

--- server.py
+++ server.py
@@ -1,5 +1,13 @@
+import os
 from src.app import app
 
 if __name__ == "__main__":
     app.url_map.strict_slashes = False
-    app.run(host="0.0.0.0", debug=True, port=5000)
+
+    DEBUG = os.getenv("DEBUG", "False").lower() in ("true", "1")
+
+    app.run(
+        host="0.0.0.0",
+        debug=DEBUG,
+        port=5000,
+    )
