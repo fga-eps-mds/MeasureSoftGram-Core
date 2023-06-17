962ab8c3f2dbd5f732b4c1c8dbc4563405598c2d
marcosgtavares <marcosgabriel.ro@hotmail.com>
2023-06-16 23:09:56 -0300

    refactor: rename max_values to normalized_weights
    Co-authored-by: Jonathan Oliveira <jonathanjorge8445@gmail.com>
    Co-authored-by: Hilmer Neri <hilmer@unb.br>

--- src/core/agregation.py
+++ src/core/agregation.py
@@ -5,5 +5,5 @@ def aggregation_operation(weighted_value, weights):
     weighted_value = np.array(weighted_value)
     weights = np.array(weights)
     normalized_value = np.linalg.norm(weighted_value)
-    max_value = np.linalg.norm(weights)
-    return normalized_value / max_value
+    normalized_weights = np.linalg.norm(weights)
+    return normalized_value / normalized_weights
